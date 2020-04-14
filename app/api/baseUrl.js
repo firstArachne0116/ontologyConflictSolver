b2fa402a7692614f6faa2131fb19751c58332aa2
abc <firstArachne0116@gmail.com>
2020-04-15 07:23:46 +1000

    https

--- app/api/baseUrl.js
+++ app/api/baseUrl.js
@@ -1,2 +1,2 @@
-export default baseUrl = 'http://shark.sbs.arizona.edu/ontology-conflict-solver-server/dev/';
+export default baseUrl = 'https://shark.sbs.arizona.edu/ontology-conflict-solver-server/dev/';
 // export default baseUrl = 'http://shark.sbs.arizona.edu/ontology-conflict-solver-server/dev/';
